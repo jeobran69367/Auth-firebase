b13416f870877abffdee5e72e300a919d70c185d
Jeobran Kombou <jeobrannathanael@gmail.com>
2024-03-28 16:43:07 +0100

    first

---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    projectId: 'travauxsisters-user',
+    appId: '1:774680204644:web:7c7bd0b105162a1cd11671',
+    storageBucket: 'travauxsisters-user.appspot.com',
+    apiKey: 'AIzaSyBfgEcgbjGbZelMxrXklZhJKI5TCVyWf54',
+    authDomain: 'travauxsisters-user.firebaseapp.com',
+    messagingSenderId: '774680204644',
+  },
+};
